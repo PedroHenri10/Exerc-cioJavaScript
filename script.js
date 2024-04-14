f4fd50989bb87ab8acac7159c8ab90240d13052c
PedroHenri10 <160438856+PedroHenri10@users.noreply.github.com>
2024-04-14 20:19:21 +0000

    Acrescentei mais código js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const nome = prompt("Digite seu nome: ")
 alert('olá, ' + nome+ ', esse é meu primeiro código em JavaScript!')
+const idade = prompt("Digite sua idade: ")
+alert('sua idade é ' + idade+ ' anos!')
 
