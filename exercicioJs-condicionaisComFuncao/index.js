e609bb971fcd0924a08776f3a09a89f999f8cf31
PedroHenri10 <160438856+PedroHenri10@users.noreply.github.com>
2024-04-22 20:06:42 -0300

    Update index.js

--- exercicioJs-condicionaisComFuncao/index.js
+++ exercicioJs-condicionaisComFuncao/index.js
@@ -1,1 +1,16 @@
+function verificarSenha(usuario, senha){
+  if(usuario === "Pedro" && senha === 1234){
+     return true;
+}else{
+    return false;
+}
+}
 
+let usuario = "Pedro";
+let senha = 1234;
+let verificacao = verificarSenha(usuario, senha);
+if(verificacao){
+  console.log("usuário entrou!");
+}else{
+  console.log("usuário ou senha incorretos!");
+}
