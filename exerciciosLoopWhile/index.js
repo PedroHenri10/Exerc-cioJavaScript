8a752f4e896d6c77944b04852358b3f2b917e5db
PedroHenri10 <160438856+PedroHenri10@users.noreply.github.com>
2024-04-29 19:12:58 -0300

    colocando conteudo no index.js

--- exerciciosLoopWhile/index.js
+++ exerciciosLoopWhile/index.js
@@ -1,1 +1,12 @@
+let cidades = ["Barueri", "Santana de Parnaíba", "Osasco", "São Paulo"];
+for (let x in cidades){
+  console.log(cidades[x]);
+}
 
+console.log("");
+
+let n = 1
+while(n <=100){
+  console.log(n);
+  n++;
+}
