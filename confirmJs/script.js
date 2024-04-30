9d714492096621aa6db2aa0e2859192a9f90e216
PedroHenri10 <160438856+PedroHenri10@users.noreply.github.com>
2024-04-30 20:23:24 -0300

    Add files via upload

---
+++ confirmJs/script.js
@@ -0,0 +1,7 @@
+const EPalmeirense = confirm('olá! Você é palmeirense?')
+
+if(EPalmeirense){
+  alert("Parabéns! você torce para o maior campeão nacional, e você é feliz!!!")
+}else{
+ alert("Putz, deve ser muito ruim não ser palmeirense")
+}
