4d6a2df0d528167d1841a7d1dc16d299e2756b5a
PedroHenri10 <160438856+PedroHenri10@users.noreply.github.com>
2024-06-07 21:47:02 -0300

    Update Index.js

--- CriandoCampofornulariocomJs/Index.js
+++ CriandoCampofornulariocomJs/Index.js
@@ -1,1 +1,13 @@
+const input = document.querySelector('input');
+const lista = document.querySelector('ul');
 
+function handleKeyUp(e){
+ if (e.key === 'Enter'){
+   const newLi = document.createElement('li');
+   newLi.innerHTML = input.value;
+   lista.appendChild(newLi);
+   input.value = '';
+ }
+}
+
+input.addEventListener('keyup', handleKeyUp);
