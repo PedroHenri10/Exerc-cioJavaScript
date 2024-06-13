8e6a7d3743554c444d52d0a2c796cebd43f8feeb
PedroHenri10 <160438856+PedroHenri10@users.noreply.github.com>
2024-06-12 21:04:43 -0300

    Update Index.js

--- SomaNaMatrizJs/Index.js
+++ SomaNaMatrizJs/Index.js
@@ -1,1 +1,18 @@
+function criarMatrizAleatoria() {
+  const matriz = [];
 
+  // Cria a matriz 10x10
+  for (let i = 0; i < 10; i++) {
+    matriz[i] = [];
+    for (let j = 0; j < 10; j++) {
+      // Gera um número aleatório entre 1 e 99
+      matriz[i][j] = Math.floor(Math.random() * 99) + 1; 
+    }
+  }
+
+  return matriz;
+}
+
+// Chama a função e imprime a matriz
+const matrizAleatoria = criarMatrizAleatoria();
+console.log(matrizAleatoria);
