6fa61ab6a13845fff0093df84162a89af41529c2
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2024-07-11 23:58:02 -0300

    Update Index.js

--- PromptComOnClick/Index.js
+++ PromptComOnClick/Index.js
@@ -1,1 +1,11 @@
+const novoparagrafo = document.getElementById('myElement');
 
+alertnaTela() {
+ let name = prompt("Qual é o seu nome?");
+ let idade = prompt("qual a sua idade?");
+
+ if (idade >= 18){
+   novoparagrafo.innerHTML = "Olá, " + name + "! Você pode dirigir.";
+ }else{
+   novoparagrafo.innerHTML = "Olá, " + name + "! Você não pode dirigir.";
+     }
