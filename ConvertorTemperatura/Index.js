4d6f917e6906197bc99f2fddcd0069e1213665b4
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2024-07-13 23:53:46 -0300

    Update Index.js atividade finalizada

--- ConvertorTemperatura/Index.js
+++ ConvertorTemperatura/Index.js
@@ -1,1 +1,13 @@
+const spantemperatura = document.getElementById('temperatura-convertida');
 
+function celsiusParaFahrenheit(celsius) {
+  // Fórmula de conversão: Fahrenheit = (Celsius * 9/5) + 32
+  let fahrenheit = (celsius * 9/5) + 32;
+  return fahrenheit;
+}
+
+let tempCelsius = parseInt(prompt("Qual é a temperatura em graus Celsius na sua cidade?"));
+
+let tempFahrenheit = celsiusParaFahrenheit(tempCelsius);
+
+spantemperatura.innerHTML = `A temperatura da sua cidade em Fahrenheit é igual a: ${tempFahrenheit}`;
