f1766e267c23c9f552f5c1af94ed62aea3ee76ca
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2024-07-22 21:58:59 -0300

    Update Index.js

--- Contadorcomonclickingerativo/Index.js
+++ Contadorcomonclickingerativo/Index.js
@@ -1,1 +1,20 @@
+let numero = document.getElementById('numero');
 
+const btnincremento = document.getElementById('incremento');
+const btndecremento = document.getElementById('decremento');
+
+function incremento(){
+   let resultado = parseInt(numero.textContent);
+   resultado++;
+   numero.textContent = resultado;
+}
+
+function decremento(){
+   let resultado = parseInt(numero.textContent);
+   resultado--;
+   numero.textContent = resultado;
+}
+
+btnincremento.addEventListener('click', incremento);
+
+btndecremento.addEventListener('click', decremento);
