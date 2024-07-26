009a0b4b516496b98f11f5af6ad6b1e1869c03a8
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2024-07-26 20:15:56 -0300

    Exercício número primo

--- exerciciosLoopWhile/index.js
+++ exerciciosLoopWhile/index.js
@@ -4,15 +4,17 @@ for (let x in cidades){
 }
 
 console.log("");
+console.log("contador de 1 até 100");
 
 let n = 1
 while(n <=100){
   console.log(n);
   n++;
 }
-console.log("");
 
-// pegar um numero e ficar somando de 1 ate o próprio o numero
+console.log("");
+console.log("pegar um numero e ficar somando de 1 ate o próprio o numero");
+
 let number = 50;
 
 let i = 1;
@@ -21,4 +23,26 @@ while(i <= number){
     soma += i;
     i++;
     console.log(soma)
+}
+
+console.log("");
+console.log("verificar se o número é primo");
+let numero = 53;
+let divisores = 0;
+let i = 1;
+
+while(i <= numero){
+  if(numero % i === 0){
+    console.log(i);
+    divisores++;
+  }else{
+    divisores;
+  }
+  i++;
+}
+
+if(divisores === 2){
+  console.log(`O número: ${numero} é primo pois tem apenas dois divisores positivos 1 e ${numero}`)
+}else{
+  console.log(`O número: ${numero} não é primo pois tem mais de dois divisores positivos`)
 }
