7a66e17b324f936e76774ba2e1b9ab6b4594bb0e
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2024-07-28 18:56:17 -0300

    Concertando exercício

--- ExercícioDoWhile/script.js
+++ ExercícioDoWhile/script.js
@@ -4,11 +4,11 @@ let negativos = 0;
 do{
    let numeros = parseInt(prompt("Digite varios numeros positivos e negativos até pressionar 0 para sair do programa"));
 
-   if(numeros > 0)
+   if(numeros > 0){
       positivos++;
-   if(numeros < 0)
+  } else if(numeros < 0){
       negativos++;
-}while(numeros != 0 || numeros = "");
+}while(numeros != 0 && numeros = "");
 
 alert(`Quantidade de números positivos: ${positivos}\nQuantidade de números negativos: ${negativos}`)
 */
