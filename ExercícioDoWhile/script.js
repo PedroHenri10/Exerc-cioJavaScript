5a661fe9c8c8ed3e26fe56a44df71983c7b3b422
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2024-07-28 22:35:47 -0300

    Exercício select chamando outro Select concluído

--- ExercícioDoWhile/script.js
+++ ExercícioDoWhile/script.js
@@ -12,3 +12,34 @@ do{
 
 alert(`Quantidade de números positivos: ${positivos}\nQuantidade de números negativos: ${negativos}`)
 */
+const consoleSelect = document.getElementById('consoleSelect');
+let gameSelect = null;
+
+consoleSelect.addEventListener('change', function() {
+  let console = consoleSelect.value;
+
+  if (gameSelect) {
+    gameSelect.remove(); 
+    gameSelect = null; 
+  }
+
+  if (console === "Xbox") {
+    gameSelect = document.createElement('select');
+    gameSelect.innerHTML = `
+      <option value="">Selecione uma opção</option>
+      <option value="Forza">Forza</option>
+      <option value="Gears of War">Gears of War</option>
+      <option value="Halo">Halo</option>
+    `;
+    consoleSelect.parentNode.appendChild(gameSelect);
+  } else if (console === "PlayStation") {
+    gameSelect = document.createElement('select');
+    gameSelect.innerHTML = `
+      <option value="">Selecione uma opção</option>
+      <option value="Bloodborne">Bloodborne</option>
+      <option value="Spider-Man">Spider-Man</option>
+      <option value="Uncharted">Uncharted</option>
+    `;
+    consoleSelect.parentNode.appendChild(gameSelect);
+  }
+});
