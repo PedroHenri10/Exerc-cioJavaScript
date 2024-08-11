8892069a010d9079c6dc8f6945030595ef4fbfb8
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2024-08-10 22:27:54 -0300

    Exercício variáveis finalizado

---
+++ JohnSmilgaCourse/Exclass27.js
@@ -0,0 +1,17 @@
+/*
+## variables #1
+
+#create firstName and lastName variabels
+#assign your values
+#create address variable and assign main Street value to it
+#re-assign adress to first street later
+#log all values in console
+*/
+
+const firstName = "Pedro";
+const lastName ="Oliveira";
+
+let adress = "main street";
+adress = "first street";
+
+console.log(firstName, lastName, adress);
