9eac9ca070d7074b5c82369393429fe333d470db
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2024-08-10 23:34:35 -0300

    Desafio concatenação concluído

---
+++ JohnSmilgaCourse/Stringsconcatchallange.js
@@ -0,0 +1,15 @@
+/*
+## concatenate strings #2
+
+1 create street and country variables
+2. assign your values
+3. create fullMailingAddress variable and assign the result of street + country
+4. remember about space
+5. log fullMailingAddress in the console
+*/
+
+const street = "Osasco";
+const country = "Brasil";
+
+const fullMailingAddress = street + ' ' + country;
+console.log(fullMailingAddress);
