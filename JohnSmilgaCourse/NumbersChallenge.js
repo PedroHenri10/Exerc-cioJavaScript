89155d02fde2d2e58c9891a62de91cafc2e4bd25
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2024-08-11 22:20:56 -0300

    NumbersChallenge ex1 finalizado

---
+++ JohnSmilgaCourse/NumbersChallenge.js
@@ -0,0 +1,24 @@
+/*
+## Numbers 3
+
+1. create score1, ...2, score3 variables and assign values (1-100)
+2. calculate total score and average and assign them to the variables
+3. log theses variaboes
+
+4.  create plates variables and assign 20
+5.  create people variables and assign 7
+6.  calculate remaining plates and assign to the variable 
+7.  add one to remaining plates
+8. create massage variable and display 'there are (your value goes here) plates available' - string concatenation
+9. log message
+
+*/
+
+const score1 = 99;
+const score2 = 27;
+consg score 3 = 45;
+
+const totalscore = score1 + score2 + score3;
+const avgScore = totalscore/3;
+
+console.log(totalscore, avgScore);
