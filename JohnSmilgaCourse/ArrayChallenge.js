ce61237b8ae89c56b64195d56b92c9595d0f9a8c
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2024-08-12 11:00:49 -0300

    Array Challenge concluído

---
+++ JohnSmilgaCourse/ArrayChallenge.js
@@ -0,0 +1,16 @@
+/*
+## arrays #4
+
+1. create fruits array and store some values
+2. setup the last number as number
+3. assing the first fruit to the variable
+4. re-asding the last array item to the actual fruit
+5. log both first fruit variable and entire fruits array 
+*/
+
+const fruits = ["Apple", "avocado", "lemon", "Watermelon", 81];
+
+const firstfruit = fruits[0];
+fruits[4] = "orange";
+
+console.log(firstfruit, fruits);
