6288bc74ee7f8387c92d991195631b6fb690afc7
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2024-08-22 23:27:57 -0300

    Create Stringschallange.js

---
+++ JohnSmilgaCourse/Stringschallange.js
@@ -0,0 +1,13 @@
+/*
+
+## strings #8
+
+1. create function fullName
+2. accept two parameters "firstName", "lastName"
+3. add them together (concat) and return result in uppercase
+4. involke fullname and pass some values
+5. log results
+6. change the order of arguments
+7. refactor to object paraneters 
+
+*/
