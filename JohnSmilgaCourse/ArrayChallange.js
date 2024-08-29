918087f80b94a516f7c0c7b4af6953610086f6f9
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2024-08-28 21:58:39 -0300

    Preparando o desafio para amanhã pois agora eu vou dormir

---
+++ JohnSmilgaCourse/ArrayChallange.js
@@ -0,0 +1,27 @@
+/* ## Array Methods #9
+
+Multiple Videos
+
+#### Create Students Array
+
+1. setup students array with 5 students
+   {
+   id: 1,
+   name: 'peter',
+   score: 80,
+   favoriteSubject: 'math',
+   },
+
+#### Use Multiple Files
+
+- easer to manage code base (file, project)
+- order matters
+- other ways to achieve the same result (ES6 modules, fetch DB )
+- not limited to just static data
+
+#### updatedStudents
+
+1. add role:'student' property to each object
+   using MAP method
+2. assign to 'updatedStudents' variable and log
+*/
