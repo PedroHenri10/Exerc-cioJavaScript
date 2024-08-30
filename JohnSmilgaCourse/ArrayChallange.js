01853ac9069b68041c2e76fb44a47ef0b1e5c794
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2024-08-29 22:35:27 -0300

    Criando array com 5 itens

--- JohnSmilgaCourse/ArrayChallange.js
+++ JohnSmilgaCourse/ArrayChallange.js
@@ -25,3 +25,34 @@ Multiple Videos
    using MAP method
 2. assign to 'updatedStudents' variable and log
 */
+const students = [{
+   id: 1,
+   name: 'peter',
+   score: 80,
+   favoriteSubject: 'math',
+},
+{
+   id: 2,
+   name: 'Halaand',
+   score: 90,
+   favoriteSubject: 'physical education',
+},
+{
+   id: 3,
+   name: 'Mary',
+   score: 87,
+   favoriteSubject: 'physics',
+},
+{
+   id: 4,
+   name: 'Luka',
+   score: 54,
+   favoriteSubject: 'Biology',
+},          
+{
+   id: 5,
+   name: 'Mario',
+   score: 77,
+   favoriteSubject: 'Geography',
+},
+];
